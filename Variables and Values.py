f6ab6e54ef8aea37aa988ac6447484d1010e33bb
YasharNmadar <63202790+YasharNmadar@users.noreply.github.com>
2020-11-22 13:59:08 +0330

    Add files via upload

---
+++ Variables and Values.py
@@ -0,0 +1,15 @@
+intVar = 10
+floatVar = 12.5
+boolVar = True
+StrVar = "Hello World"
+listVar = [1,5,8]
+tupleVar = ("Python", "Programmer")
+dictVar = {"Name": "Yashar", "Age": 13, "isStudent":True}
+
+print("intVar = "+str((intVar)))
+print("floatVar = {0}".format(floatVar))
+print("boolVar = %s" %boolVar)
+print("StrVar = {0}".format(StrVar))
+print("listVar = {0}".format(listVar))
+print("tupleVar = {0}".format(tupleVar))
+print("dictVar = {0}".format(dictVar))
